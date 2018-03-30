6d55796ffa6fa1e46a66cc7f1df6d482600a33ff
denkquer <36424493+denkquer@users.noreply.github.com>
2018-03-30 23:33:00 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,9 +2,6 @@
 var tgröße = 15;
 var reihen = [];
 
-
-
-
 function setup() {
   createCanvas(innerWidth, innerHeight-4);
   for(var i=0;i*tgröße<width;i++){
@@ -15,49 +12,68 @@ function setup() {
 
 function draw() {
   background(0);
+
   for(var i=0;i<reihen.length;i++){
+    reihen[i].update();
     reihen[i].show();
+    if(reihen[i].flag == -1){
+      reihen.splice(i,1);
+      console.log("hi");
+    }
   }
 }
 
 function Zeichen(x,y,num) {
   this.x = x;
   this.duh = y;
-
   this.value = String.fromCharCode(0x30A0 + round(random(0, 96)));
 
   this.show = function(){
-    num==0?fill(130,255,130,):fill(0,255,0);
+    num==0?fill(150,255,150):fill(0,255,0);
     frameCount%round(random(60*0.5,60*20))==0?this.value = String.fromCharCode(0x30A0 + round(random(0, 96))):0;
     round(random(0,10000))==1?this.value = round(random(0,9)):0;
+    if(num==0 && frameCount%round(random(72))==0){
+      this.value = String.fromCharCode(0x30A0 + round(random(0, 96)));
+    }
     textSize(tgröße);
     this.y = reihen[y].y-num*tgröße;
     text(this.value,this.x,this.y);
   }
-
 }
 
 function Reihe(x){
   this.x = x*tgröße;
-  this.y = 0;
-  this.länge = round(random(6,66));
+  this.y = -10;
+  this.länge = round(random(6,70));
   this.parts = [];
-  this.speed = random(1,50)/10;
+  this.speed = random(15,70)/10;
+  this.flag = false;
 
   this.ini = function(){
 
     for(var i=0;i<this.länge;i++){
       this.parts.push(new Zeichen(this.x,x,i));
+      this.flag = true;
     }
   }
 
   this.show = function(){
+
     for(var i=0;i<this.parts.length;i++){
       this.parts[i].show();
     }
-    this.y+=this.speed;
   }
 
+  this.update = function(){
+    this.y+=this.speed;
 
+    if(this.flag == false){
+      this.ini();
+    }
 
+    if(this.y-this.länge*tgröße>height){
+      this.parts = [];
+      reihen.splice(x,1,new Reihe(x));
+    }
+  }
 }
