4c969fa82ae5e9faf5da21f1b47f0ae8343a08fa
denkquer <36424493+denkquer@users.noreply.github.com>
2018-04-09 14:13:04 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var tgröße = 16;
 var reihen = [];
 
 function setup() {
-  createCanvas(innerWidth, innerHeight-4);
+  createCanvas(innerWidth, innerHeight);
   for(var i=0;i*tgröße<width;i++){
     reihen.push(new Reihe(i));
     reihen[i].ini();
